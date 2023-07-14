e331f406177a8dcbe151dbc55fb0f5034c14dddf
saerom <sagesaeromkim@gmail.com>
2023-07-14 15:46:42 +0900

    Design: 헤더 푸터 디자인 적용

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <p>Everything is connected.</p>;
+  return (
+    <p className="text-gray-400 text-center px-2 py-6">
+      {"Everything is connected | All rights reserved"}
+    </p>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center p-4">
       <Link href="/">
-        <h1>{"Saerom's blog"}</h1>
+        <h1 className="text-2xl font-bold">{"Saerom's blog"}</h1>
       </Link>
-      <nav>
+      <nav className="flex gap-4">
         <Link href="/">home</Link>
         <Link href="about">about</Link>
         <Link href="posts">posts</Link>
