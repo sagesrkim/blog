1928c0b1b65620eb1ef304abdd29ac0405f9e4b1
saerom <sagesaeromkim@gmail.com>
2023-08-17 16:22:03 +0900

    Feat: 메인 페이지 info 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 export default function HomePage() {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-3xl font-bold mt-2">
+    <section className="text-center min-h-screen  p-16">
+      <h2 className="text-3xl font-bold mt-4 mb-20">
         {"꾸준함과 커뮤니케이션을 높은 가치로 두는 개발자 김새롬입니다"}
       </h2>
-      <h3 className="text-2xl mt-4">Frontend Developer</h3>
-
-      <p className="mx-40 text-left">
+      <div className="mx-40 text-left">
         꾸준함과 커뮤니케이션에 중요한 가치를 두고 있습니다. <br />
         공부한 내용과 문제 해결 경험을 지속적으로 블로그에 작성하고 팀
         프로젝트로 협업을 경험했습니다. <br />
@@ -15,7 +13,7 @@ export default function HomePage() {
         깔끔한 UI를 구현하는 것을 좋아합니다. UI와 UX를 꼼꼼하게 살필 뿐만
         아니라, 사용자의 입장에서 웹을 탐색하며 느꼈던 사소한 경험을 개선하고
         최적화된 성능을 제공하는 개발자가 되고 싶습니다.
-      </p>
+      </div>
     </section>
   );
 }
