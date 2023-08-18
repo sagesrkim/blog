6c0533a10052241e0822040a606d3769816ad6f3
saerom <sagesaeromkim@gmail.com>
2023-08-18 16:31:47 +0900

    Feat: 아카이빙 페이지 생성

--- src/app/archiving/page.tsx
+++ src/app/archiving/page.tsx
@@ -1,3 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+import velogLogo from "../../../public/assets/velogLogo.png";
+
 export default function ArchivePage() {
-  return <div>깃허브 저장소와 블로그 </div>;
+  return (
+    <>
+      <section className="p-20 flex flex-row justify-center">
+        <div className="flex flex-row m-10">
+          <Link className="mx-4" href="https://github.com/sagesrkim" passHref>
+            <BsGithub className="text-7xl" />
+          </Link>
+          <div>
+            <h3>GitHub 저장소</h3>
+            <p>
+              GitHub 저장소입니다. 주요 프로젝트와 알고리즘 문제 풀이를 기록하고
+              있습니다.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-row m-10">
+          <Link className="mx-4" href="https://velog.io/@sagesrkim" passHref>
+            <Image src={velogLogo} alt="velog" width={80} height={80} />
+          </Link>
+          <div>
+            <h3>블로그</h3>
+            <p>
+              React, TypeScript, Next.js를 배우고 프로젝트를 진행하며 겪었던
+              경험을 기록하고 사고하는 기술 블로그입니다.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
