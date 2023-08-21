e65e292c032039bdfd96bddf748e881794e2187e
saerom <sagesaeromkim@gmail.com>
2023-08-21 19:04:56 +0900

    Design: 프로필 이미지 수정 및 디자인 변경

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,34 +1,38 @@
 import Profile from "@/components/Profile";
 
-
 export default function AboutPage() {
-  const TITLE_CLASS = "text-xl font-bold text-gray-800 m-2";
+  const TITLE_CLASS = "text-xl font-bold text-gray-800 m-2 flex";
+  const INFO_CLASS = "m-2 text-xl";
+  const SPAN_CLASS = "text-2xl font-bold mb-2";
   return (
     <>
       <Profile />
-      <section className="text-center mx-20">
-        <h2 className={TITLE_CLASS}>이름</h2>
-        <p>김새롬 </p>
-        <h2 className={TITLE_CLASS}>연락처</h2>
-        <a
-          href="mailto:sagesaeromkim@gmail.com"
-          className="hover:text-blue-900 hover:underline"
-        >
-          sagesaeromkim@gmail.com
-        </a>
-        <p></p>
-        <h2 className={TITLE_CLASS}>기술 스택</h2>
-        <div className="flex flex-row flex-wrap justify-center items-center ">
-          <span className="mx-10">React</span>
-          <span className="mx-10">Next.js</span>
-          <span className="mx-10">TypeScript</span>
-          <span className="mx-10">Redux-Toolkit</span>
+      <section className="text-center mx-20 px-20 flex flex-col justify-center items-center">
+        <div className="flex">
+          <h2 className={TITLE_CLASS}>이름</h2>
+          <p className={INFO_CLASS}>김새롬 </p>
         </div>
-        <div className="flex flex-row flex-wrap justify-center items-center ">
-          <span className="mx-10">TailwindCSS</span>
-          <span className="mx-10">JavaScript</span>
-          <span className="mx-10">HTML5</span>
-          <span className="mx-10">CSS3</span>
+        <div className="flex">
+          <h2 className={TITLE_CLASS}>연락처</h2>
+          <a
+            href="mailto:sagesaeromkim@gmail.com"
+            className={`${INFO_CLASS} hover:text-blue-900 hover:underline`}
+          >
+            sagesaeromkim@gmail.com
+          </a>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-xl font-bold text-gray-800 m-2">기술 스택</h2>
+          <div className=" flex flex-col bg-slate-100 p-10 rounded-xl drop-shadow-xl hover:translate-y-2 duration-300">
+            <span className={SPAN_CLASS}>React</span>
+            <span className={SPAN_CLASS}>Next.js</span>
+            <span className={SPAN_CLASS}>TypeScript</span>
+            <span className={SPAN_CLASS}>Redux-Toolkit</span>
+            <span className={SPAN_CLASS}>TailwindCSS</span>
+            <span className={SPAN_CLASS}>JavaScript</span>
+            <span className={SPAN_CLASS}>HTML5</span>
+            <span className={SPAN_CLASS}>CSS3</span>
+          </div>
         </div>
         <a href="mailto:sagesaeromkim@gmail.com">
           <button className="bg-blue-500 rounded-full px-4 py-3 mt-10 font-semibold text-xl text-white hover:bg-blue-600 hover:transition-all">
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,11 +5,11 @@ export default function Profile() {
   return (
     <section className="text-center mt-10">
       <Image
-        className="w-50 rounded-full mx-auto p-4"
+        className="rounded-lg mx-auto"
         src={profileImage}
         alt="profileImage"
-        height={200}
-        width={200}
+        height={300}
+        width={300}
         priority
       />
       <h3 className="text-2xl mt-2 font-semibold">Frontend Developer</h3>
