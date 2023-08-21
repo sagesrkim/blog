c508070a2c8879bd2be304993fdb125b3246a644
saerom <sagesaeromkim@gmail.com>
2023-08-21 19:58:23 +0900

    Design: 링크 컴포넌트 hover 디자인 변경

--- src/app/archiving/page.tsx
+++ src/app/archiving/page.tsx
@@ -8,7 +8,11 @@ export default function ArchivePage() {
     <>
       <section className="p-20 flex flex-row justify-center">
         <div className="flex flex-row m-10">
-          <Link className="mx-4" href="https://github.com/sagesrkim" passHref>
+          <Link
+            className="mx-4 hover:translate-y-2 hover:duration-300"
+            href="https://github.com/sagesrkim"
+            passHref
+          >
             <BsGithub className="text-7xl" />
           </Link>
           <div>
@@ -20,11 +24,15 @@ export default function ArchivePage() {
           </div>
         </div>
         <div className="flex flex-row m-10">
-          <Link className="mx-4" href="https://velog.io/@sagesrkim" passHref>
+          <Link
+            className="mx-4 hover:translate-y-2 hover:duration-300 "
+            href="https://velog.io/@sagesrkim"
+            passHref
+          >
             <Image src={velogLogo} alt="velog" width={80} height={80} />
           </Link>
           <div>
-            <h3 className="text-2xl font-bold">Velog</h3>
+            <h3 className="text-2xl font-bold ">Velog</h3>
             <p>
               React, TypeScript, Next.js를 배우고 프로젝트를 진행하며 겪었던
               경험을 기록하고 사고하는 기술 블로그입니다.
