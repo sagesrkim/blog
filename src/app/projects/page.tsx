1c3f183202347e24e50ab7679489037c9c0421ed
saerom <sagesaeromkim@gmail.com>
2023-08-22 21:36:05 +0900

    Feat: 프로젝트 페이지 생성

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,18 @@
 export default function ProjectPage() {
-  return <div>projects</div>;
+  return (
+    <>
+      <section>
+        <main>
+          <h3>모두의 여행</h3>
+          <p>
+            개발 기간: 2023.02 ~ 2023.05 기술 스택: `Next.js`, `React`,
+            `TypeScript`, `Redux Toolkit`, `Styled Component`, `Post CSS`,
+            `Spring`, `AWS` - 배포 주소:
+            [http://mohaeng.site](http://mohaeng.site/) - 프로젝트 주소:
+            https://github.com/sagesrkim/Mohaeng
+          </p>
+        </main>
+      </section>
+    </>
+  );
 }
