f66700272f8b7ad21362976204a6b9cbe7e6f8b1
saerom <sagesaeromkim@gmail.com>
2023-09-05 16:53:05 +0900

    Feat: 글로벌 폰트 적용

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,14 +3,27 @@ export default function ProjectPage() {
     <section className="w-full">
       <main>
         <div className="m-10 p-10 bg-sky-100 rounded-lg">
-          <h3>모두의 여행</h3>
+          <h3 className="text-3xl text-center font-bold">모두의 여행</h3>
+          <div>이미지 슬라이더</div>
           <p>
-            개발 기간: 2023.02 ~ 2023.05 기술 스택: `Next.js`, `React`,
-            `TypeScript`, `Redux Toolkit`, `Styled Component`, `Post CSS`,
-            `Spring`, `AWS` - 배포 주소:
-            [http://mohaeng.site](http://mohaeng.site/) - 프로젝트 주소:
-            https://github.com/sagesrkim/Mohaeng
+            국내 여행을 보다 더 편리하고 즐겁게 할 수 있는 경험을 제공하고자
+            사용자 간 여행 정보를 교환할 수 있는 커뮤니티형 플랫폼입니다.
+            ‘모두의 여행’은 사용자가 키워드를 검색하여 여행지를 찾고 리뷰 작성
+            및 수정이 가능하고 직접 여행일정을 작성할 수 있는 서비스를 제공하고
+            있습니다. 다수 페이지의 Next.js 웹사이트를 만들면서 **라우팅, CSR,
+            SSR, SSG**에 대한 개념을 습득할 수 있었습니다. 로그인을 다루며
+            **OAuth 2.0**의 개념을 정리할 수 있었고, **UI & UX**를 지속적으로
+            고민하며 스스로 디자인을 했습니다. **백엔드 개발자와 협업**에 필요한
+            소통을 많이 배울 수 있었습니다. 필요한 API 명세를 문서로 작성하고
+            서로 공통된 지식을 함께 탐구해나가며 공부하였습니다.
           </p>
+          <p>프로젝트 주소: https://github.com/sagesrkim/Mohaeng</p>
+          <p>개발 기간: 2023.02 ~ 2023.05</p>
+          <p>
+            기술 스택: `Next.js`, `React`, `TypeScript`, `Redux Toolkit`,
+            `Styled Component`, `Post CSS`, `Spring`, `AWS`
+          </p>
+          <p> 배포 주소: [http://mohaeng.site](http://mohaeng.site/)</p>
         </div>
       </main>
     </section>
