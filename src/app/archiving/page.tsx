971d91d8804f9843d06d84699ee82f036cc1ead7
saerom <sagesaeromkim@gmail.com>
2023-09-16 16:42:04 +0900

    Feat: 링크 연결 컴포넌트 변경

--- src/app/archiving/page.tsx
+++ src/app/archiving/page.tsx
@@ -7,10 +7,14 @@ export default function ArchivePage() {
   return (
     <>
       <section className="p-20 flex flex-row justify-center space-x-0 bg-gray-100">
-        <div className="flex flex-row m-10 p-10 rounded-lg bg-white hover:translate-y-2 hover:duration-300 hover:cursor-pointer">
-          <Link className="mx-4" href="https://github.com/sagesrkim" passHref>
-            <BsGithub className="text-7xl" />
-          </Link>
+        <Link
+          href="https://github.com/sagesrkim"
+          passHref
+          className="flex flex-row m-10 p-10 rounded-lg bg-white hover:translate-y-2 hover:duration-300 hover:cursor-pointer"
+        >
+          {/* <Link className="mx-4" href="https://github.com/sagesrkim" passHref> */}
+          <BsGithub className="text-8xl mx-4" />
+          {/* </Link> */}
           <div>
             <h3 className="text-2xl font-bold">GitHub</h3>
             <p>
@@ -18,11 +22,17 @@ export default function ArchivePage() {
               있습니다.
             </p>
           </div>
-        </div>
-        <div className="flex flex-row m-10 p-10 rounded-lg bg-white hover:translate-y-2 hover:duration-300 hover:cursor-pointer">
-          <Link className="mx-4 " href="https://velog.io/@sagesrkim" passHref>
+        </Link>
+        <Link
+          href="https://velog.io/@sagesrkim"
+          passHref
+          className="flex flex-row m-10 p-10 rounded-lg bg-white hover:translate-y-2 hover:duration-300 hover:cursor-pointer"
+        >
+          {/* <Link className="mx-4 " href="https://velog.io/@sagesrkim" passHref> */}
+          <div className="mx-4 mt-4">
             <Image src={velogLogo} alt="velog" width={100} height={100} />
-          </Link>
+          </div>
+          {/* </Link> */}
           <div>
             <h3 className="text-2xl font-bold ">Velog</h3>
             <p>
@@ -30,7 +40,7 @@ export default function ArchivePage() {
               경험을 기록하고 사고하는 기술 블로그입니다.
             </p>
           </div>
-        </div>
+        </Link>
       </section>
     </>
   );
