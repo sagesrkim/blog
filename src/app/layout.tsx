7ab96d5703919f5b94bff18983cca70ec9a9d6a4
saerom <sagesaeromkim@gmail.com>
2023-10-27 22:28:38 +0900

    Fix: 메뉴 클릭시 스크롤 이동 레이아웃으로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body className="flex flex-col w-full max-w-screen-2xl mx-auto">
-        <Header />
         <main className="grow">{children}</main>
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,87 @@
-export default function HomePage() {
+"use client";
+
+import One from "@/components/One";
+import Three from "@/components/Three";
+import Two from "@/components/Two";
+import Link from "next/link";
+import React, { useRef } from "react";
+
+export default function Home() {
+  const scrollRef = useRef<(HTMLDivElement | null)[]>([null, null, null]);
+
+  const handleScrollView = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    const name = (event.target as HTMLElement).innerText;
+    const category: { [key: string]: number } = {
+      One: 0,
+      Two: 1,
+      Three: 2,
+    };
+    const targetElement = scrollRef.current[category[name]];
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <section className="text-center min-h-screen  p-16">
-      <h2 className="text-3xl font-bold mt-4 mb-20">
-        {"꾸준함과 커뮤니케이션을 높은 가치로 두는 개발자 김새롬입니다"}
-      </h2>
-      <div className="mx-40 text-left">
-        꾸준함과 커뮤니케이션에 중요한 가치를 두고 있습니다. <br />
-        공부한 내용과 문제 해결 경험을 지속적으로 블로그에 작성하고 팀
-        프로젝트로 협업을 경험했습니다. <br />
-        알기 쉽고 직관적인 UI를 제공하기 위해 동적인 코드를 작성하면서, CSS에서
-        자주 사용하는 스타일과 색상을 변수화하여 중복 코드를 줄이고 심미적이고
-        깔끔한 UI를 구현하는 것을 좋아합니다. UI와 UX를 꼼꼼하게 살필 뿐만
-        아니라, 사용자의 입장에서 웹을 탐색하며 느꼈던 사소한 경험을 개선하고
-        최적화된 성능을 제공하는 개발자가 되고 싶습니다.
+    <>
+      <div className="bg-white fixed top-0 w-full shadow-md flex justify-between items-center p-4">
+        <Link href="/">
+          <h1 className="text-2xl font-bold">{"Saerom's Portfolio"}</h1>
+        </Link>
+        <div className="flex gap-4 " onClick={handleScrollView}>
+          <span>One</span>
+          <span>Two</span>
+          <span>Three</span>
+        </div>
       </div>
-    </section>
+
+      <div>
+        <div ref={(el) => (scrollRef.current[0] = el)}>
+          <One />
+        </div>
+        <div ref={(el) => (scrollRef.current[1] = el)}>
+          <Two />
+        </div>
+        <div ref={(el) => (scrollRef.current[2] = el)}>
+          <Three />
+        </div>
+      </div>
+    </>
   );
 }
+
+// import { useRef } from "react";
+
+// export default function HomePage() {
+//   const element = useRef<HTMLDivElement>(null);
+//   const onMoveBox = () => {
+//     element.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+//   };
+//   return (
+//     <section className="text-center min-h-screen  p-16">
+//       <h2 className="text-3xl font-bold mt-4 mb-20">
+//         {"꾸준함과 커뮤니케이션을 높은 가치로 두는 개발자 김새롬입니다"}
+//       </h2>
+
+//       <div className="hover:pointer-events-auto" onClick={onMoveBox}>
+//         테스트
+//       </div>
+//       <div ref={element} className="w-full h-screen relative flex flex-col">
+//         안녕
+//       </div>
+//       <div className="mx-40 text-left">
+//         꾸준함과 커뮤니케이션에 중요한 가치를 두고 있습니다. <br />
+//         공부한 내용과 문제 해결 경험을 지속적으로 블로그에 작성하고 팀
+//         프로젝트로 협업을 경험했습니다. <br />
+//         알기 쉽고 직관적인 UI를 제공하기 위해 동적인 코드를 작성하면서, CSS에서
+//         자주 사용하는 스타일과 색상을 변수화하여 중복 코드를 줄이고 심미적이고
+//         깔끔한 UI를 구현하는 것을 좋아합니다. UI와 UX를 꼼꼼하게 살필 뿐만
+//         아니라, 사용자의 입장에서 웹을 탐색하며 느꼈던 사소한 경험을 개선하고
+//         최적화된 성능을 제공하는 개발자가 되고 싶습니다.
+//       </div>
+//     </section>
+//   );
+// }
